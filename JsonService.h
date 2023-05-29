ad90ad387f47885615ab741892cda16cc325a093
”Ivan <ivan.ivanov@gmail.com>
2023-05-29 09:12:44 +0300

    Add Json Service class

---
+++ JsonService.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <fstream>
+#include "Service.h"
+#include "json.hpp"
+
+class JsonService :public Service
+{
+public:
+	virtual Weather getWeather(std::string s) override;
+	virtual ~JsonService() {};
+};
+
